1b3adcd525c0ffbce9ce72f3a9efde221cea4ac9
abdul-kadir-uk <abdulkadiruk06@gmail.com>
2025-01-18 22:34:21 +0530

    Start Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-green-500 mt-2 text-2xl"> Chat App </h1>
+    </>
+  )
+}
+
+export default App;
+
