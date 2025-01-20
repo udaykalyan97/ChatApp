99000dc5e198e6f8873036f60437ac43c8d5da3a
uday kalyan <udaykalyan975@gmail.com>
2025-01-20 10:47:30 +0530

    chats bar frontend idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './index.css'
+import './index.css';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <>
       <h1 className="text-center text-green-500 mt-2 text-2xl"> Chat App </h1>
+      <SideBar />
     </>
   )
 }
